2af6aaeb33238c6c045aef27e5c0aaf2f0c122b9
Lxd-554629679 <554629679@qq.com>
2019-12-13 15:46:18 +0800

    创建登录页面

--- src/router/router.js
+++ src/router/router.js
@@ -1,10 +1,14 @@
 // 导入 vue-router
 import VueRouter from 'vue-router'
 import Vue from "vue"
+import login from "../views/login/login.vue"
 //注册 vue-router
 Vue.use(VueRouter)
 // 实例化
-const routes = []
+const routes = [{
+    path:'/login',
+    component:login
+}]
 const router = new VueRouter({
     //这里就是路由的配制项
     routes
